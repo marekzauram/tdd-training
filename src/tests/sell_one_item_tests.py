c999d753d2e0dae502c6ed32104a1822d9f69c1c
anti.kodas <anti.kodas@skype.net>
2013-08-26 11:46:58 +0300

    We display the price of every product as long as it's 7.95

--- src/tests/sell_one_item_tests.py
+++ src/tests/sell_one_item_tests.py
@@ -1,7 +1,23 @@
 import unittest
 
+class SalesSystem(object):
+    def on_barcode(self, barcode):
+        pass
+
+
+class Display(object):
+    def __init__(self):
+        self.text = "EUR 7.95"
+
+
 class SellOneItemTests(unittest.TestCase):
-	pass
+    def test_price_found(self):
+        sales_system = SalesSystem()
+        display      = Display()
+        #
+        sales_system.on_barcode("123")
+        #
+        self.assertEquals("EUR 7.95", display.text)
 
 
 if __name__ == "__main__":
