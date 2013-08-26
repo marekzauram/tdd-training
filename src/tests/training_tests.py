f933008e609ce1376de254c0c72e5ea16b0deeb4
anti.kodas <anti.kodas@skype.net>
2013-08-26 10:31:35 +0300

    Traning tests added to repo

---
+++ src/tests/training_tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TDDriven(unittest.TestCase):
+    """ Testcases for training
+    """
+
+    def test_training(self):
+        '''I am testing          
+        '''        
+        self.assertEqual(1, 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
