af3bf33b96d5b62181cce13b9598a9dc34a6f42c
anti.kodas <anti.kodas@skype.net>
2013-08-26 12:11:20 +0300

    Preparations for scanning different barcodes and getting different prices

--- src/tests/sell_one_item_tests.py
+++ src/tests/sell_one_item_tests.py
@@ -1,23 +1,35 @@
 import unittest
 
 class SalesSystem(object):
+    def __init__(self, display):
+        self.display = display
+
     def on_barcode(self, barcode):
-        pass
+        self.display.text = "EUR 7.95"
 
 
 class Display(object):
     def __init__(self):
-        self.text = "EUR 7.95"
+        self.text = None
 
 
 class SellOneItemTests(unittest.TestCase):
     def test_price_found(self):
-        sales_system = SalesSystem()
         display      = Display()
-        #
+        sales_system = SalesSystem(display)
+
         sales_system.on_barcode("123")
-        #
+
         self.assertEquals("EUR 7.95", display.text)
+
+    def test_another_price_found(self):
+        self.skipTest("This does not work just yet")
+        display      = Display()
+        sales_system = SalesSystem(display)
+
+        sales_system.on_barcode("321")
+
+        self.assertEquals("EUR 10.00", display.text)
 
 
 if __name__ == "__main__":
