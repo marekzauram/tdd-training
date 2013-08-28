060e826731e4d04148741062ad21dbae070d6045
martin.roos <martin.roos@skype.net>
2013-08-28 09:36:22 +0300

    removed duplications in sales report tests

--- src/tests/sell_one_item_tests.py
+++ src/tests/sell_one_item_tests.py
@@ -142,6 +142,16 @@ class PrintingTests(unittest.TestCase):
         )
 
 class ReportingTests(unittest.TestCase):
+    def setUp(self):
+        self.products = [
+            {'price': 20.00, 'tax': 'G',  'barcode': '6666'},
+            {'price': 10.00, 'tax': 'GP', 'barcode': '5555'},
+        ]
+        self.display = Display()
+        self.printer = Printer()
+        self.current_time = '2013-12-01 12:13'
+        self.sales_system = SalesSystem(self.display, self.printer, self.products)
+
     def test_empty_report(self):
         sales_system = SalesSystem(None, None, None)
         current_time = '2013-01-01 12:13'
@@ -152,43 +162,28 @@ class ReportingTests(unittest.TestCase):
         )
 
     def test_one_sale_report(self):
-        products = [
-            {'price': 20.00, 'tax': 'G',  'barcode': '6666'},
-        ]
-        display = Display()
-        printer = Printer()
-        current_time = '2013-12-01 12:13'
-        sales_system = SalesSystem(display, printer, products)
-        sales_system.on_barcode('6666')
-        sales_system.save_sale(current_time)
-        self.assertEquals(sales_system.get_sales_report(current_time),
-            'Sales report at %s' % current_time         + '\n' +
+        self.sales_system.on_barcode('6666')
+        self.sales_system.save_sale(self.current_time)
+        self.assertEquals(self.sales_system.get_sales_report(self.current_time),
+            'Sales report at %s' % self.current_time         + '\n' +
             '"Date", "Subtotal", "GST", "PST", "Total"' + '\n' +
-            '"%s", "%.2f", "%.2f", "%.2f", "%.2f"' % (current_time, 20.00, 1.60, 0.0, 21.60) + '\n' +
+            '"%s", "%.2f", "%.2f", "%.2f", "%.2f"' % (self.current_time, 20.00, 1.60, 0.0, 21.60) + '\n' +
             '"Total", "20.00", "1.60", "0.00", "21.60"' + '\n'
         )
         
     def test_triple_sale_report(self):
-        products = [
-            {'price': 20.00, 'tax': 'G',  'barcode': '6666'},
-            {'price': 10.00, 'tax': 'GP', 'barcode': '5555'},
-        ]
-        display = Display()
-        printer = Printer()
-        current_time = '2013-12-01 12:13'
-        sales_system = SalesSystem(display, printer, products)
-        sales_system.on_barcode('6666')
-        sales_system.save_sale(current_time)
-        sales_system.on_barcode('5555')
-        sales_system.save_sale(current_time)
-        sales_system.on_barcode('5555')
-        sales_system.save_sale(current_time)
-        self.assertEquals(sales_system.get_sales_report(current_time),
-            'Sales report at %s' % current_time         + '\n' +
+        self.sales_system.on_barcode('6666')
+        self.sales_system.save_sale(self.current_time)
+        self.sales_system.on_barcode('5555')
+        self.sales_system.save_sale(self.current_time)
+        self.sales_system.on_barcode('5555')
+        self.sales_system.save_sale(self.current_time)
+        self.assertEquals(self.sales_system.get_sales_report(self.current_time),
+            'Sales report at %s' % self.current_time         + '\n' +
             '"Date", "Subtotal", "GST", "PST", "Total"' + '\n' +
-            '"%s", "%.2f", "%.2f", "%.2f", "%.2f"' % (current_time, 20.00, 1.60, 0.0, 21.60) + '\n' +
-            '"%s", "%.2f", "%.2f", "%.2f", "%.2f"' % (current_time, 10.00, 0.80, 0.50, 11.30) + '\n' +
-            '"%s", "%.2f", "%.2f", "%.2f", "%.2f"' % (current_time, 10.00, 0.80, 0.50, 11.30) + '\n' +
+            '"%s", "%.2f", "%.2f", "%.2f", "%.2f"' % (self.current_time, 20.00, 1.60, 0.0, 21.60) + '\n' +
+            '"%s", "%.2f", "%.2f", "%.2f", "%.2f"' % (self.current_time, 10.00, 0.80, 0.50, 11.30) + '\n' +
+            '"%s", "%.2f", "%.2f", "%.2f", "%.2f"' % (self.current_time, 10.00, 0.80, 0.50, 11.30) + '\n' +
             '"Total", "40.00", "3.20", "1.00", "44.20"' + '\n'
         )
         
